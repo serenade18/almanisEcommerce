50b7fde585794dd522a105c783e5b46dabd96f02
Ahmed Sereta <seretaahmed18@gmail.com>
2024-09-01 17:07:40 +0300

    Designed search field

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import '../header/Header.css';
+import icons from '../../constants/icons';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
     <>
       <header>
-        <div className="container-fluid"></div>
+        <div className="container-fluid">
+            <div className="row align-items-center">
+                <div className="col-sm-1">
+                    <img src={icons.Logo} />
+                </div>
+
+                {/* header serch section */}
+                <div className="col-sm-6">
+                    <div className="headersearch d-flex align-items-center mt-2">
+                        <div className="selectDrop cursor-pointer">
+                            All Categories
+                        </div>
+                        <div className="search d-flex align-items-center">
+                            <input
+                                type='text'
+                                placeholder='Search for items....'
+                            />
+                            <SearchIcon/>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
       </header>
     </>
   )
--- src/constants/icons.js
+++ src/constants/icons.js
@@ -2,10 +2,12 @@ import Wish from '../assets/images/svgs/icon-heart.svg';
 import User from '../assets/images/svgs/icon-user.svg';
 import Cart from '../assets/images/svgs/icon-cart.svg';
 import Refresh from '../assets/images/svgs/icon-compare.svg';
+import Logo from '../assets/images/logo/almalogo.svg'
 
 export default {
     Wish,
     User,
     Cart,
-    Refresh
+    Refresh,
+    Logo
 }
