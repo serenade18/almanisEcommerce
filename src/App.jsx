c76742a498607daadc24d49a8fc6ef0600e6d9c9
TOGREE06 <togree@TOGREE06s-MBP.example.com>
2024-09-07 10:33:37 +0300

    Added general pages routs

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import './App.css'
 import Layout from './hoc/Layout';
 import icons from './constants/icons';
 
-// lazy load pages
+// lazy load general pages
 const Home = lazy(() => import('./pages/general/home/Home'))
 const About = lazy(() => import('./pages/general/about/About'))
+const Shop = lazy(() => import('./pages/general/shop/Shop'))
+const Partner = lazy(() => import('./pages/general/partner/Partner'))
+const Contact = lazy(() => import('./pages/general/contact/Contact'))
 
 function App() {
 
@@ -26,6 +29,9 @@ function App() {
           {/* general pages */}
           <Route exact={true} path="/" element={<Layout><Home /></Layout>}/>
           <Route exact={true} path="/about" element={<Layout><About /></Layout>}/>
+          <Route exact={true} path="/shop" element={<Layout><Shop /></Layout>}/>
+          <Route exact={true} path="/partner" element={<Layout><Partner /></Layout>}/>
+          <Route exact={true} path="/contact" element={<Layout><Contact /></Layout>}/>
         </Routes>
       </Suspense>
     </Router>
